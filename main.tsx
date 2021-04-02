eea30f9304ca5e16cfcdee4c7f5f45745a74e7ac
佐藤昭文 <akfm.sato@gmail.com>
2021-04-02 23:49:29 +0900

    update handle callback

--- main.tsx
+++ main.tsx
@@ -1,10 +1,14 @@
-import { jsx, serve } from "https://deno.land/x/sift@0.1.7/mod.ts";
+import {
+  jsx,
+  serve,
+  validateRequest,
+} from "https://deno.land/x/sift@0.1.7/mod.ts";
 import { html } from "https://cdn.skypack.dev/htm@v3.0.4/preact/standalone.module.js";
 
 const App = html`
   <div>
     <h1>Hello world!</h1>
-    <p>this page is testing...</p>
+    <p>page: index</p>
   </div>
 `;
 
@@ -26,6 +30,27 @@ const App = html`
 //   return new Response("hello world");
 // }
 
+const handleIndexRequest = async (request: Request) => {
+  // Make sure the request is a GET request.
+  const { error } = await validateRequest(request, {
+    GET: {},
+  });
+  // validateRequest populates the error if the request doesn't meet
+  // the schema we defined.
+  if (error) {
+    return jsx(html`
+      <div>internal error</div>
+    `);
+  }
+  // const markdown = await fetch(
+  //   "https://raw.githubusercontent.com/AkifumiSato/deno_markdown_docs/main/README.md",
+  // ).then((res) => res.text());
+  // console.log(markdown);
+
+  // Return all the quotes.
+  return jsx(App);
+};
+
 serve({
-  "/": (_request: Request) => jsx(App),
+  "/": handleIndexRequest,
 });
