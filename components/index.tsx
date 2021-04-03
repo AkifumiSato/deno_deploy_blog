1eccf43a46d45f332b2db4848f2ccbd36470f133
佐藤昭文 <akfm.sato@gmail.com>
2021-04-03 14:03:50 +0900

    index page add

---
+++ components/index.tsx
@@ -0,0 +1,7 @@
+import { h } from "https://cdn.skypack.dev/preact@10.5.13";
+
+export const IndexPage = () => (
+  <div>
+    <h1>Index Page</h1>
+  </div>
+);
--- main.tsx
+++ main.tsx
@@ -3,54 +3,10 @@ import {
   serve,
   validateRequest,
 } from "https://deno.land/x/sift@0.1.7/mod.ts";
-import { html } from "https://cdn.skypack.dev/htm@v3.0.4/preact/standalone.module.js";
-
-const App = html`
-  <div>
-    <h1>Hello world!</h1>
-    <p>page: index</p>
-  </div>
-`;
-
-// todo
-// const handleRequest = async (request: Request) => {
-// const { error, body } = await validateRequest(request, {
-//   GET: {
-//     params: ["title"],
-//   },
-//   POST: {
-//     body: ["title", "artist", "album", "released", "genres"],
-//   },
-// });
-// if (error) {
-//   return json({ error: error.message }, { status: error.status });
-// }
-// const markdown = await fetch('https://raw.githubusercontent.com/AkifumiSato/deno_markdown_docs/main/README.md').then(res => res.text());
-// console.log(markdown);
-//   return new Response("hello world");
-// }
-
-const handleIndexRequest = async (request: Request) => {
-  // Make sure the request is a GET request.
-  const { error } = await validateRequest(request, {
-    GET: {},
-  });
-  // validateRequest populates the error if the request doesn't meet
-  // the schema we defined.
-  if (error) {
-    return jsx(html`
-      <div>internal error</div>
-    `);
-  }
-  // const markdown = await fetch(
-  //   "https://raw.githubusercontent.com/AkifumiSato/deno_markdown_docs/main/README.md",
-  // ).then((res) => res.text());
-  // console.log(markdown);
-
-  // Return all the quotes.
-  return jsx(App);
-};
+import { h } from "https://cdn.skypack.dev/preact@10.5.13";
+import { IndexPage } from './components/index.tsx'
+// import markdown from "https://cdn.skypack.dev/markdown-wasm@1.1.2";
 
 serve({
-  "/": handleIndexRequest,
+  "/": (_request: Request) => jsx(<IndexPage />),
 });
